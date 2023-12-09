0b57cacab664f487e1da674ffdee9edb8461e1f9
Homero Hurtado <homerohg28@gmail.com>
2023-12-09 10:42:05 -0600

    search algoritm

--- js/script.js
+++ js/script.js
@@ -6,38 +6,38 @@ const elementBtn = document.getElementById("myButton");
 
 /* Desplazamiento del header */
 
-window.addEventListener("scroll",function(){
-    var header=document.querySelector("header");
-    header.classList.toggle("abajo",window.scrollY>0);
+window.addEventListener("scroll", function () {
+    var header = document.querySelector("header");
+    header.classList.toggle("abajo", window.scrollY > 0);
 })
 
 /* Cuando el documento este completamenta cargado ejecutar la funcion */
 
-document.addEventListener("DOMContentLoaded", function () { 
+document.addEventListener("DOMContentLoaded", function () {
 
-   /* Renderizar vertical u horizontalmente basandonos en la variable de control */
+    /* Renderizar vertical u horizontalmente basandonos en la variable de control */
 
-    if(!controlView){
-    elementList.setAttribute("class", "vertical");
-    }else{
+    if (!controlView) {
+        elementList.setAttribute("class", "vertical");
+    } else {
         elementList.setAttribute("class", "horizontal");
     }
-    });
+});
 
-elementBtn.addEventListener("click", function(){
+elementBtn.addEventListener("click", function () {
 
     /* Si es true la pasa a false y si es false lo pasa a true */
 
-    controlView = !controlView; 
+    controlView = !controlView;
 
-    if(!controlView){
+    if (!controlView) {
         elementList.setAttribute("class", "vertical");
-        }else{
-            elementList.setAttribute("class", "horizontal");
-        }
+    } else {
+        elementList.setAttribute("class", "horizontal");
+    }
 });
 
-switch(controlView){
+switch (controlView) {
     case controlView: // si controlView == true
         elementList.setAttribute("class", "horizontal");
         break;
@@ -50,38 +50,6 @@ switch(controlView){
 
 }
 
-// Filtrado de busqueda
-
-document.getElementById("inputSearch").addEventListener("keyup", internal_search);
-
-function internal_search(){
-
-    filter = inputSearch.value.toUpperCase();
-    li =  optionsSearch.getElementsByTagName("li");
-
-    //Recorriendo elementos a filtrar mediante los "li"
-
-    for (i = 0; i < li.length; i++){
-
-        a = li[i].getElementsByTagName("a")[0];
-        textValue = a.textContent || a.innerText;
-
-        if(textValue.toUpperCase().indexOf(filter) > -1){
-            li[i].style.display = "";
-            optionsSearch.style.display = "block";
-
-            if(inputSearch.value === ""){
-                optionsSearch.style.display = "none";
-            }
-
-        } else{
-            li[i].style.display = "none";
-        }
-    }
-
-}
-
-//Buscador de contenido//
 
 //Ejecutar funciones
 
@@ -90,32 +58,55 @@ document.getElementById("cover-ctn-search").addEventListener("click", hide_input
 
 //Declaracion de variables
 
-bars_search= document.getElementById("box-search");
+bars_search = document.getElementById("box-search");
 cover_search = document.getElementById("cover-ctn-search");
 inputSearch = document.getElementById("inputSearch");
 optionsSearch = document.getElementById("options-search");
 
 // Funcion para mostrar el buscador
 
-function show_input(){
-    bars_search.style.visibility= "visible";
-    bars_search.style.left="1px"
-    cover_search.style.display="block"
+function show_input() {
+    bars_search.style.visibility = "visible";
+    cover_search.style.display = "block"
+    bars_search.style.left = "1px"
     inputSearch.focus();
 
-    if(inputSearch.value === ""){
-        optionsSearch.style.display= "none";
+    if (inputSearch.value === "") {
+        optionsSearch.style.display = "none";
     }
 }
 
 //Funcion para ocultar el buscador
 
-function hide_input(){
-    bars_search.style.visibility="hidden";
-    cover_search.style.display="none";
-    inputSearch.value="";
+function hide_input() {
+    bars_search.style.visibility = "hidden";
+    cover_search.style.display = "none";
+    inputSearch.value = "";
     optionsSearch.style.display = "none";
 }
 
 
+/* Busqueda */
+
+document.addEventListener('DOMContentLoaded', function () {
+    const inputSearch = document.getElementById('inputSearch');
+    const elements = document.querySelectorAll('.element');
+
+    inputSearch.addEventListener('input', function () {
+        const searchTerm = inputSearch.value.toLowerCase();
+
+        elements.forEach(element => {
+            const nameElement = element.dataset.name.toLowerCase();
+            const isEqual = nameElement.includes(searchTerm);
+
+            if (isEqual) {
+                element.style.display = 'block';
+            } else {
+                element.style.display = 'none';
+            }
+
+        })
+    })
+
+})
 
